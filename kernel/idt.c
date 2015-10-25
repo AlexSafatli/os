9b411816ae0facd2270e1e89492d4164e30b068d
Alex Safatli <safatli@cs.dal.ca>
2015-10-25 15:53:30 -0300

    Finish implementing simple IDT functions

--- kernel/idt.c
+++ kernel/idt.c
@@ -10,10 +10,23 @@ idt_ptr_t   idt;
 void idt_init() {
   
   // Set table entries.
-  memset(&idt_entries, 0, sizeof(idt_entry_t) * IDT_NUM_ENTRIES);
+  memset(&idt_entries, 0, sizeof(idt_entry_t) * IDT_NUM_ENTRIES);  
 
   // Set IDT Metadata.
   idt.limit   = (sizeof(idt_entry_t) * IDT_NUM_ENTRIES) - 1;
   idt.address = (unsigned int)&idt_entries;
+
+}
+
+void idt_set_entry(int pos, unsigned int offset, unsigned short selector, 
+  unsigned char flags) {
+
+  idt_entires[pos].offset_0_15  = offset & 0xFFFF;
+  idt_entries[pos].offset_16_31 = (offset >> 16) & 0xFFFF;
+  idt_entries[pos].selector     = selector;
+  idt_entries[pos].reserved     = 0;
+  // @todo Uncomment OR below when we get to user-mode.
+  // It sets the interrupt gate's level to 3.
+  idt_entries[pos].flags        = flags /* | 0x60 */;
 
 }
