b05c14f67dbb815dd81042a94276804512bb268d
Alex Safatli <safatli@cs.dal.ca>
2015-11-16 17:56:45 -0400

    Geh...

--- kernel/timer.c
+++ kernel/timer.c
@@ -10,8 +10,8 @@ static void timer(cpu_state_t cpu, stack_state_t stack) {
 
 void timer_init(unsigned int freq) {
   
-  interrupt_install(PIC1_START, &timer);
   unsigned int divisor = PIT_CLOCK_SPEED / freq;
+  interrupt_install(PIC1_START, &timer);
   outb(PIT_CMD_PORT, PIT_REPEAT);
   outb(PIT_DATA_PORT1, divisor & 0xFF);
   outb(PIT_DATA_PORT1, (divisor >> 8) & 0xFF);
--- kernel/timer.h
+++ kernel/timer.h
@@ -13,7 +13,7 @@
 #define PIT_DATA_PORT2 0x42
 #define PIT_CMD_PORT   0x43
 #define PIT_REPEAT     0x36
-#define PIT_CLOCK_SPEED 1193180 /* Hz */
+#define PIT_CLOCK_SPEED 1193182 /* Hz */
 
 void timer_init(unsigned int);
 
