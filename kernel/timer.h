e680e1b399a26229bc833ad5a6f559dc235d6fb5
Alex Safatli <safatli@cs.dal.ca>
2015-11-18 21:09:43 -0400

    Fixed some macros

--- kernel/timer.h
+++ kernel/timer.h
@@ -9,11 +9,11 @@
 #include "interrupts.h"
 #include "idt.h"
 
-#define PIT_DATA_PORT1 0x40
-#define PIT_DATA_PORT2 0x42
-#define PIT_CMD_PORT   0x43
-#define PIT_REPEAT     0x36
-#define PIT_CLOCK_SPEED 1193182 /* Hz */
+#define PIT_DATA_PORT1  0x40
+#define PIT_DATA_PORT2  0x42
+#define PIT_CMD_PORT    0x43
+#define PIT_REPEAT      0x36
+#define PIT_CLOCK_SPEED 1193180 /* Hz */
 
 void timer_init(unsigned int);
 
